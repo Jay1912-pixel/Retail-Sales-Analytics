4ea836e03fe57f98a5b6d932a5bef24ae7692c34
Jay Kumar Patel <jay.2023ug2015@iiitranchi.ac.in>
2026-09-23 01:59:12 +0530

    Update rfm_segmentation.sql

--- sql/rfm_segmentation.sql
+++ sql/rfm_segmentation.sql
@@ -47,12 +47,32 @@ SELECT
     f_score,
     m_score,
     (r_score + f_score + m_score) AS rfm_total,
+
     CASE
-        WHEN r_score >= 4 AND f_score >= 4 AND m_score >= 4 THEN 'Champions'
-        WHEN r_score >= 3 AND f_score >= 3 THEN 'Loyal Customers'
-        WHEN r_score >= 4 AND f_score <= 2 THEN 'New Customers'
-        WHEN r_score <= 2 AND f_score >= 3 THEN 'At Risk'
-        WHEN r_score <= 2 AND f_score <= 2 AND m_score <= 2 THEN 'Lost'
+        WHEN r_score >= 4
+             AND f_score >= 4
+             AND m_score >= 4
+            THEN 'Champions'
+
+        WHEN r_score >= 3
+             AND f_score >= 3
+             AND m_score >= 3
+            THEN 'Loyal Customers'
+
+        WHEN r_score >= 4
+             AND f_score <= 2
+            THEN 'Recent Customers'
+
+        WHEN r_score <= 2
+             AND f_score >= 3
+            THEN 'At Risk'
+
+        WHEN r_score <= 2
+             AND f_score <= 2
+             AND m_score <= 2
+            THEN 'Lost'
+
         ELSE 'Needs Attention'
     END AS segment
+
 FROM rfm_scores;
